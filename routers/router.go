a38750ba4b24ab93dce3da312833b4a8eb59d023
Austin Bisharat <austinbisharat@gmail.com>
2017-02-04 14:17:53 -0800

    move all routes into router.go

--- routers/router.go
+++ routers/router.go
@@ -1,11 +1,27 @@
 package routers
 
 import (
+	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"github.com/leo-backend/controllers"
+	"github.com/leo-backend/core/authentication"
 )
 
 func InitRoutes() *mux.Router {
 	router := mux.NewRouter()
-	router = SetAuthenticationRoutes(router)
+	router.HandleFunc("/login", controllers.Login).Methods("POST")
+	router.Handle("/refresh-token-auth",
+		negroni.New(
+			negroni.HandlerFunc(authentication.RequireTokenAuthentication),
+			negroni.HandlerFunc(controllers.RefreshToken),
+		)).Methods("GET")
+	router.Handle("/logout",
+		negroni.New(
+			negroni.HandlerFunc(authentication.RequireTokenAuthentication),
+			negroni.HandlerFunc(controllers.Logout),
+		)).Methods("GET")
+	router.HandleFunc("/user", controllers.GetUser).Methods("POST")
+	router.HandleFunc("/register", controllers.Register).Methods("POST")
+	router.HandleFunc("/hello", controllers.HelloController).Methods("GET")
 	return router
 }
