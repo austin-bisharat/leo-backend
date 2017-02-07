fbaff24a6bae70873d0390f63260410d9ab6a22c
Justin Kotalik <t-jukota@microsoft.com>
2017-02-06 18:16:57 -0800

    Fixing API

--- routers/router.go
+++ routers/router.go
@@ -10,7 +10,7 @@ func InitRoutes() *mux.Router {
 	router.HandleFunc("/login", controllers.Login).Methods("POST")
 	router.HandleFunc("/refresh_token_auth", controllers.RefreshToken).Methods("GET")
 	router.HandleFunc("/logout", controllers.Logout).Methods("POST")
-	router.HandleFunc("/user", controllers.GetUser).Methods("POST")
+	router.HandleFunc("/get_user", controllers.GetUser).Methods("POST")
 	router.HandleFunc("/create_user", controllers.CreateUser).Methods("POST")
 	router.HandleFunc("/hello", controllers.HelloController).Methods("GET")
 	router.HandleFunc("/register", controllers.Register).Methods("POST")
